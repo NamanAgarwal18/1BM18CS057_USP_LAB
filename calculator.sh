ece878d2e09b271ec6b5eaf54053db42cf34ee73
NamanAgarwal18 <53811147+NamanAgarwal18@users.noreply.github.com>
2020-10-08 15:17:24 +0530

    Add files via upload

---
+++ calculator.sh
@@ -0,0 +1,13 @@
+echo "Enter the two numbers: "
+read a
+read b
+echo "$a + $b =  "
+ expr $a + $b
+echo "$a - $b =  "
+expr $a - $b
+echo "$a * $b =  "
+expr $a \* $b
+echo "$a / $b =  "
+expr $a / $b
+echo "$a % $b =  "
+expr $a % $b
